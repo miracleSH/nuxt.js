0e99d683bafc6c6a630d82b8790671d4b1dfa07f
SH <https://github.com/miracleSH>
2022-07-09 14:22:45 +0900

    서버 폴더 생성 및 asyncData 속성 사용

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <h1>{{$route.name}}페이지</h1>
       <NuxtLink to="/">홈페이지</NuxtLink>
       <NuxtLink to="/main">메인페이지</NuxtLink>
-      <NuxtLink to="/product">메인페이지</NuxtLink>
+      <NuxtLink to="/product">상품페이지</NuxtLink>
     </header>
     
     <Nuxt/>
--- pages/main.vue
+++ pages/main.vue
@@ -2,12 +2,25 @@
   <div>
     <h1>메인페이지</h1>
     <p>메인페이지 입니다.</p>
+    <div>
+      {{products}}
+    </div>
   </div>
 </template>
 <script>
-
+import axios from 'axios'
 export default {
-  name: 'MainPage'
+  name: 'MainPage', 
+  async asyncData() {
+    const response = await axios.get('http://localhost:8080/products')
+    const products = response.data
+    return { products }
+  },
+  data(){
+    return{
+      
+    }
+  },
 }
 </script>
 <style scoped>
